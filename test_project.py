0d9c8e1f9ae1c6018ee7b95dd929cc64a6907c0d
rrodrigues3 <r.v.r2000@hotmail.com>
2024-01-16 15:41:36 +0000

    alterações quase finais

---
+++ test_project.py
@@ -0,0 +1,31 @@
+from project import ler_sensor_humidade, ler_sensor_mosca, ler_sensor_temperatura, notificar_mosca, contar_moscas_por_armadilha
+from project import adicionar_armadilha, mostrar_detalhes_armadilhas, mostrar_numero_armadilhas, alterar_nome_armadilha
+from project import apagar_armadilha, avaliar_risco_presenca_mosca
+
+
+def test_ler_sensor_mosca_esta_dentro_do_intervalo():
+    resultado = ler_sensor_mosca()
+    assert 0 <= resultado <= 5
+
+def test_ler_sensor_temperatura_esta_dentro_do_intervalo():
+    resultado = ler_sensor_temperatura()
+    assert 15 <= resultado <= 30
+
+def test_ler_sensor_humidade_esta_dentro_do_intervalo():
+    resultado = ler_sensor_humidade()
+    assert 30 <= resultado <= 80
+
+def test_notificar_mosca_output_formato_correto():
+    # Verificando se a função produz a saída no formato esperado
+    contagem_moscas = 5
+    nome_armadilha = "Armadilha1"
+    coordenadas = (10, 20)
+    temperatura = 25
+    humidade = 60
+
+    expected_output = (
+        f"Foi detectada a presença de {contagem_moscas} moscas da azeitona na armadilha '{nome_armadilha}' nas coordenadas {coordenadas}.\n"
+        f"Condições ambientais - Temperatura: {temperatura}°C, Humidade: {humidade}%.\n"
+    )
+
+
